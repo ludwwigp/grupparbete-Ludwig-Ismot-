5b5f790a7badd55542c11e691abd5c8a40e814e9
ludwwigp <ludwig-pettersson@live.com>
2023-10-06 15:27:44 +0200

    23-10-06

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById("color-button").addEventListener("click", function () {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+  });
+  
+  function getRandomColor() {
+    return "#" + Math.floor(Math.random()*100000).toString(16);
+  }
+
+  
+
+
+
+
+
+
+
+
+  
